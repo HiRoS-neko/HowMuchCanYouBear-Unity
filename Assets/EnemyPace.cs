1756c02886e35f1a18ac8588a1f0c68757ed19a7
HiRoS <hiros.neko@gmail.com>
2018-03-09 20:28:40 -0500

    Added Pacing Script
    Lets the human pace randomly within a distance from the original point

---
+++ Assets/EnemyPace.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.XR.WSA;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyPace : MonoBehaviour
+{
+    [SerializeField] private float _distance;
+
+    private Vector3 _pos;
+
+    private Animator _anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Magnitude(transform.position - _pos) > _distance)
+        {
+            _anim.SetBool("turn", true);
+        }
+        else
+        {
+            _anim.SetBool("turn", false);
+        }
+    }
+}
